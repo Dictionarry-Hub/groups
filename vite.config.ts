b8534a20218c030156b8bf8c585355a5e5e4f3e2
Sam Chau <chaucted@gmail.com>
2024-12-20 23:58:11 +1030

    chore: update path aliases for hooks in tsconfig and vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@utils": path.resolve(__dirname, "./src/utils"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
     },
   },
 });
