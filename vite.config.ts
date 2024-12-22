f864b80a42803e8296b4c086e268f90a684e8c19
Sam Chau <chaucted@gmail.com>
2024-12-23 04:06:41 +1030

    refactor: consolidate TypeScript configuration; remove tsconfig.app.json and update paths in tsconfig.json and tsconfig.node.json

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "./src"),
       "@utils": path.resolve(__dirname, "./src/utils"),
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@components": path.resolve(__dirname, "./src/components"),
